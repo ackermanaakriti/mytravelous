ef49a1dac9f392d9198822be788485fb01dddf74
ackermanaakriti <bhandariaakriti897@gmail.com>
2022-04-21 16:32:26 +0545

    first

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Body from './Components/Body';
 
 import { BrowserRouter as Router , Routes, Route} from 'react-router-dom';
 import Navbar from './Components/Navbar';
-import Cardcontainer from './Cardcontainer';
+import Cardcontainer from './Components/Cardcontainer';
 
 
 function App() {
--- src/Components/Cardcontainer.js
+++ src/Components/Cardcontainer.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import Card from './Components/Card';
-import './Components/Cardcontainer.css'
+
+import Card from './Card';
+
 
 
 function Cardcontainer() {
   return (
     <div className='card-container'>
-    <div className='mycards'>
+    
         <h1 className='heading'>POPULAR PLACES  </h1>
+        <div className='mycards'>
         <Card/>
         <Card/>
         <Card/>
